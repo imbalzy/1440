4c6f88b58b19b1432e73eaaa25420ef63508786b
imbalzy <34955962+imbalzy@users.noreply.github.com>
2021-04-18 17:50:10 -0400

    push

--- src/main/java/agent/Agent2.java
+++ src/main/java/agent/Agent2.java
@@ -25,70 +25,76 @@ public class Agent2 extends AbsLSVM18Agent implements IAgent {
 	private Map<String, Double> regionalAvg;
 	private Map<String, Double> lastBids;
 	private Map<Double, Double> avgPrice;
+
 	public Agent2(String name) {
 		super(name);
 		lastBids = new HashMap<>();
 		// TODO: fill this in (if necessary)
 		this.regionalAvg = new HashMap<>();
-		this.regionalAvg.put("A",30.0/18);
-		this.regionalAvg.put("B",40.0/18);
-		this.regionalAvg.put("C",45.0/18);
-		this.regionalAvg.put("D",45.0/18);
-		this.regionalAvg.put("E",40.0/18);
-		this.regionalAvg.put("F",30.0/18);
-		this.regionalAvg.put("G",35.0/18);
-		this.regionalAvg.put("H",50.0/18);
-		this.regionalAvg.put("I",55.0/18);
-		this.regionalAvg.put("J",55.0/18);
-		this.regionalAvg.put("K",50.0/18);
-		this.regionalAvg.put("L",35.0/18);
-		this.regionalAvg.put("M",30.0/18);
-		this.regionalAvg.put("N",40.0/18);
-		this.regionalAvg.put("O",45.0/18);
-		this.regionalAvg.put("P",45.0/18);
-		this.regionalAvg.put("Q",40.0/18);
-		this.regionalAvg.put("R",30.0/18);
-		this.avgPrice=new HashMap<>();
-		this.avgPrice.put(1.1,1.);
+		this.regionalAvg.put("A", 30.0 / 18);
+		this.regionalAvg.put("B", 40.0 / 18);
+		this.regionalAvg.put("C", 45.0 / 18);
+		this.regionalAvg.put("D", 45.0 / 18);
+		this.regionalAvg.put("E", 40.0 / 18);
+		this.regionalAvg.put("F", 30.0 / 18);
+		this.regionalAvg.put("G", 35.0 / 18);
+		this.regionalAvg.put("H", 50.0 / 18);
+		this.regionalAvg.put("I", 55.0 / 18);
+		this.regionalAvg.put("J", 55.0 / 18);
+		this.regionalAvg.put("K", 50.0 / 18);
+		this.regionalAvg.put("L", 35.0 / 18);
+		this.regionalAvg.put("M", 30.0 / 18);
+		this.regionalAvg.put("N", 40.0 / 18);
+		this.regionalAvg.put("O", 45.0 / 18);
+		this.regionalAvg.put("P", 45.0 / 18);
+		this.regionalAvg.put("Q", 40.0 / 18);
+		this.regionalAvg.put("R", 30.0 / 18);
+		this.avgPrice = new HashMap<>();
+		this.avgPrice.put(1.1, 1.);
 	}
-	
+
 	@Override
 	protected Map<String, Double> getBids(Map<String, Double> minBids) {
 		// TODO: fill this in
-		
+
 		Map<String, Double> bids = new HashMap<>();
-		if (this.getCurrentRound()==0) {
+		if (this.getCurrentRound() == 0) {
 			for (String s : getProximity()) {
-				bids.put(s, this.getValuation(s)*1.3);
+				bids.put(s, Math.min(1.261, this.getValuation(s) * 1));
 			}
-		}
-		else {
+		} else {
 			HashSet<String> myGoods = new HashSet<>();
 			for (String s : getProximity()) {
-				if (this.getTentativeAllocation().contains(s)&& !this.lastBids.containsKey(s)) {
+				if (this.getTentativeAllocation().contains(s) && !this.lastBids.containsKey(s)) {
 					myGoods.add(s);
 				}
 			}
 			if (this.isNationalBidder()) {
-				for (String s : getProximity()) if (!myGoods.contains(s)) {
-					double originalV = this.getValuation(myGoods);
-					myGoods.add(s);
-					double newV = this.getValuation(myGoods);
-					if (originalV+minBids.get(s)<newV) {
-						bids.put(s,minBids.get(s));
+				for (String s : getProximity())
+					if (!myGoods.contains(s)) {
+						double originalV = this.getValuation(myGoods);
+						myGoods.add(s);
+						double newV = this.getValuation(myGoods);
+						if (originalV + minBids.get(s) - 1.25 < newV) {
+							bids.put(s, minBids.get(s) + 0.01);
+						}
+						myGoods.remove(s);
 					}
-					myGoods.remove(s);
-				}
-			}else {
-				for (String s : getProximity()) if (!myGoods.contains(s)) {
-					double originalV = this.getValuation(myGoods);
-					myGoods.add(s);
-					double newV = this.getValuation(myGoods);
-					if (originalV+minBids.get(s)<newV) {
-						bids.put(s,minBids.get(s));
+			} else {
+				for (String s : getProximity())
+					if (!myGoods.contains(s)) {
+						double originalV = this.getValuation(myGoods);
+						myGoods.add(s);
+						double newV = this.getValuation(myGoods);
+						if (originalV + minBids.get(s) + 0.011 < newV) {
+							if (originalV + minBids.get(s) + 1.25 > newV)
+								bids.put(s, newV - originalV);
+							else
+								bids.put(s, minBids.get(s) + 0.01);
+
+						}
+						myGoods.remove(s);
 					}
-					myGoods.remove(s);
-				}
 			}
 		}
 		this.lastBids = bids;
@@ -107,15 +113,14 @@ public class Agent2 extends AbsLSVM18Agent implements IAgent {
 			List<List<ITradeMessage>> tradeHistory) {
 		// TODO: fill this in (if necessary)
 		this.lastBids.clear();
-		
 
 	}
-
 
 	public static void main(String[] args) throws InterruptedException {
 		if (args.length == 0) {
 			// Don't change this.
-			new OfflineSimulation("offline_test_config.json", "input_configs/lsvm_smra_offline.json", "outfile", false).run();
+			new OfflineSimulation("offline_test_config.json", "input_configs/lsvm_smra_offline.json", "outfile", false)
+					.run();
 		} else {
 			// Don't change this.
 			MyLSVM18Agent agent = new MyLSVM18Agent(NAME);
--- src/main/java/agent/MyLSVM18Agent.java
+++ src/main/java/agent/MyLSVM18Agent.java
@@ -1,6 +1,7 @@
 package agent;
 
-import java.io.IOException;import java.time.Instant;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -22,104 +23,112 @@ public class MyLSVM18Agent extends AbsLSVM18Agent implements IAgent {
 	private final static String NAME = "NM"; // TODO: give your agent a name.
 
 	private Map<String, Double> regionalAvg;
-	private Map<String, Double> lastBids;
-	private Map<Double, Double> avgPrice;
+	private Set<String> lastAllo;
+	private List<String> best4;
+
 	public MyLSVM18Agent(String name) {
 		super(name);
-		lastBids = new HashMap<>();
+		lastAllo = new HashSet<>();
 		// TODO: fill this in (if necessary)
 		this.regionalAvg = new HashMap<>();
-		this.regionalAvg.put("A",30.0/18);
-		this.regionalAvg.put("B",40.0/18);
-		this.regionalAvg.put("C",45.0/18);
-		this.regionalAvg.put("D",45.0/18);
-		this.regionalAvg.put("E",40.0/18);
-		this.regionalAvg.put("F",30.0/18);
-		this.regionalAvg.put("G",35.0/18);
-		this.regionalAvg.put("H",50.0/18);
-		this.regionalAvg.put("I",55.0/18);
-		this.regionalAvg.put("J",55.0/18);
-		this.regionalAvg.put("K",50.0/18);
-		this.regionalAvg.put("L",35.0/18);
-		this.regionalAvg.put("M",30.0/18);
-		this.regionalAvg.put("N",40.0/18);
-		this.regionalAvg.put("O",45.0/18);
-		this.regionalAvg.put("P",45.0/18);
-		this.regionalAvg.put("Q",40.0/18);
-		this.regionalAvg.put("R",30.0/18);
-		this.avgPrice=new HashMap<>();
-		this.avgPrice.put(30./18,16.8);
-		this.avgPrice.put(35./18,19.6);
-		this.avgPrice.put(40./18,20.6);
-		this.avgPrice.put(45./18,18.2);
-		this.avgPrice.put(50./18,21.7);
-		this.avgPrice.put(55./18,22.4);
+		this.regionalAvg.put("A", 24.0 / 18);
+		this.regionalAvg.put("B", 32.0 / 18);
+		this.regionalAvg.put("C", 36.0 / 18);
+		this.regionalAvg.put("D", 36.0 / 18);
+		this.regionalAvg.put("E", 32.0 / 18);
+		this.regionalAvg.put("F", 30.0 / 18);
+		this.regionalAvg.put("G", 35.0 / 18);
+		this.regionalAvg.put("H", 50.0 / 18);
+		this.regionalAvg.put("I", 55.0 / 18);
+		this.regionalAvg.put("J", 55.0 / 18);
+		this.regionalAvg.put("K", 50.0 / 18);
+		this.regionalAvg.put("L", 35.0 / 18);
+		this.regionalAvg.put("M", 30.0 / 18);
+		this.regionalAvg.put("N", 40.0 / 18);
+		this.regionalAvg.put("O", 45.0 / 18);
+		this.regionalAvg.put("P", 45.0 / 18);
+		this.regionalAvg.put("Q", 40.0 / 18);
+		this.regionalAvg.put("R", 30.0 / 18);
 	}
-	
+
 	@Override
 	protected Map<String, Double> getBids(Map<String, Double> minBids) {
 		// TODO: fill this in
-		
+
 		Map<String, Double> bids = new HashMap<>();
-		if (this.getCurrentRound()==0) {
+		if (this.getCurrentRound() == 0) {
 			for (String s : getProximity()) {
-				bids.put(s, this.getValuation(s)*1);
+				bids.put(s, Math.min(1.261, this.getValuation(s) * 1));
 			}
-		}
-		else {
+			if (!this.isNationalBidder()) {
+				for (String s : minBids.keySet()) {
+					if (getValuation(s) != 0
+							&& Math.pow((getValuation(s) - 3) / 17, regionalAvg.get(s) / 5 * 4) >= 0.9) {
+						this.best4.add(s);
+					}
+				}
+
+			}
+		} else {
 			HashSet<String> myGoods = new HashSet<>();
 			for (String s : getProximity()) {
-				if (this.getTentativeAllocation().contains(s)&& !this.lastBids.containsKey(s)) {
+				if ((this.getTentativeAllocation().contains(s) && this.lastAllo.contains(s))) {
+					myGoods.add(s);
+				} else if (this.getCurrentRound() >= 2 && this.best4.contains(s)
+						&& this.getTentativeAllocation().contains(s)) {
 					myGoods.add(s);
 				}
 			}
-			if (this.isNationalBidder()) {
-				for (String s : getProximity()) if (!myGoods.contains(s)) {
+
+			for (String s : getProximity())
+				if (!myGoods.contains(s)) {
 					double originalV = this.getValuation(myGoods);
 					myGoods.add(s);
 					double newV = this.getValuation(myGoods);
-					if (originalV+minBids.get(s)<newV) {
-						bids.put(s,minBids.get(s)+0.01);
+					if (originalV + minBids.get(s) + 0.011 < newV) {
+						if (originalV + minBids.get(s) + 1.25 > newV)
+							bids.put(s, newV - originalV);
+						else
+							bids.put(s, minBids.get(s) + 0.011);
+
 					}
 					myGoods.remove(s);
-				}
-			}else {
-				for (String s : getProximity()) if (!myGoods.contains(s)) {
-					double originalV = this.getValuation(myGoods);
-					myGoods.add(s);
+				} else {
 					double newV = this.getValuation(myGoods);
-					if (originalV+minBids.get(s)<newV) {
-						bids.put(s,minBids.get(s)+0.01);
-					}
 					myGoods.remove(s);
+					double originalV = this.getValuation(myGoods);
+					if (originalV + minBids.get(s) < newV) {
+						bids.put(s, minBids.get(s) + 0.011);
+					}
+					myGoods.add(s);
 				}
-			}
+
 		}
-		this.lastBids = bids;
+		this.lastAllo = new HashSet<>(this.getTentativeAllocation());
 		return bids;
 	}
 
 	@Override
 	protected void onAuctionStart() {
 		// TODO: fill this in (if necessary)
-		this.lastBids.clear();
-
+		this.lastAllo.clear();
+		this.best4 = new ArrayList<>();
 	}
 
-//	@Override
+	// @Override
 	protected void onAuctionEnd(Map<Integer, Set<String>> allocations, Map<Integer, Double> payments,
 			List<List<ITradeMessage>> tradeHistory) {
 		// TODO: fill this in (if necessary)
-		this.lastBids.clear();
-		
+		this.lastAllo.clear();
 
 	}
-	
+
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		if (args.length == 0) {
 			// Don't change this.
-			new OfflineSimulation("offline_test_config.json", "input_configs/lsvm_smra_offline.json", "outfile", false).run();
+			new OfflineSimulation("offline_test_config.json", "input_configs/lsvm_smra_offline.json", "outfile", false)
+					.run();
 		} else {
 			// Don't change this.
 			MyLSVM18Agent agent = new MyLSVM18Agent(NAME);
